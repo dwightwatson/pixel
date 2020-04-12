cbe099fe7377b080970ec050dc22cc30ee154b2b
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-04-12 20:05:55 +1000

    boilerplate

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies
-  })
+const fs = require('fs');
+const sharp = require('sharp');
+
+module.exports = async (req, res) => {
+  const stream = fs.createReadStream(__dirname + '/example.jpg');
+
+  res.setHeader('Content-Type', 'image/jpeg');
+  res.setHeader('Cache-Control', 'max-age=31536000, public');
+
+  const transformation = sharp()
+    .rotate()
+    .resize(200, 200);
+
+  stream.pipe(transformation).pipe(res);
 }
