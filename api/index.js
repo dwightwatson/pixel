d62f0f0c25cd9b2c5a4c3049a28161313035db36
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-04-13 12:04:51 +1000

    dynamic

--- api/index.js
+++ api/index.js
@@ -4,12 +4,15 @@ const sharp = require('sharp');
 module.exports = async (req, res) => {
   const stream = fs.createReadStream(__dirname + '/example.jpg');
 
+  const height = parseInt(req.query.height, 10) || 200;
+  const width = parseInt(req.query.width, 10) || 200;
+
   res.setHeader('Content-Type', 'image/jpeg');
   res.setHeader('Cache-Control', 'max-age=31536000, public');
 
   const transformation = sharp()
     .rotate()
-    .resize(200, 200);
+    .resize(height, width);
 
   stream.pipe(transformation).pipe(res);
 }
